b7c9a308df79e3c69dd2a22f6b907b99018d7211
soler marjorie <soler.marjorie81@gmail.com>
2025-03-24 11:35:23 +0100

    from register

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Account;
+use App\Repository\AccountRepository;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class RegisterController extends AbstractController
+{
+    public function __construct(
+        private readonly AccountRepository $accountRepository,
+        private readonly EntityManagerInterface $em
+    ){}
+
+
+    #[Route('/register', name: 'app_register_addaccount')]
+    public function addAccount(Request $request): Response
+    {
+        $msg = "";
+        $type = "";
+
+        //Créer un onjet account
+        $account = new Account();
+        //Créer un objet register type (formulaire)
+        $form = $this->createForm(RegisterType::class, $account);
+        //Récypérer le résulat de la requête 
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+           
+           //Test si le compte existe déjà
+           if(!$this->accountRepository->findOneBy(["email"=> $account->getEmail()])){
+                $account->setRoles(['ROLE_USER']);
+                $this->em->persist($account);
+                $this->em->flush();
+                $msg = "Votre compte a été créé avec succès";
+                $type = "success";
+
+           } 
+           else{
+                $msg = "Ce compte existe déjà";
+                $type = "danger";
+           }
+           $this->addFlash($type, $msg);
+        }
+
+        //Passer à la vue
+        return $this->render('register/addAccount.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
